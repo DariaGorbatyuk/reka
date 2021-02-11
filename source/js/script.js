95f1e5d2e5b143a871d64eba7381540600972da8
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-02-11 20:56:06 +0300

    добавляет слайдер portfolio

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 
@@ -12,4 +13,17 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
   }
+});
+*/
+$(document).ready(function () {
+  $('.portfolio__slider').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    speed: 1000,
+    easing: 'ease',
+    infinite: true,
+    centerPadding: '13%',
+ /*   autoplay: true,
+    autoplaySpeed: 3000*/
+  });
 });
